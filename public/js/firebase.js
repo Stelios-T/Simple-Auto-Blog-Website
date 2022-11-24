348c989beef2eaae624d3d657821c04499d15027
Stelios-T <67365815+Stelios-T@users.noreply.github.com>
2022-11-24 18:34:54 +0200

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,12 +1,8 @@
-// Firebase project configuration
+
+// TODO: Replace the following with your app's Firebase project configuration
 let firebaseConfig = {
-  apiKey: "AIzaSyC61QdeTI288q_6FYmrEE2UJIkbQ3YaMRg",
-  authDomain: "project-db0c9.firebaseapp.com",
-  projectId: "project-db0c9",
-  storageBucket: "project-db0c9.appspot.com",
-  messagingSenderId: "687553068893",
-  appId: "1:687553068893:web:94c271ffad176b9704b2b4"
-  };
+  //...
+};
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
